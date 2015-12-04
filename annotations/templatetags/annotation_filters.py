f105547992a4ccfa0c969dae819ca656970d1b6c
John Shen <john.da.shen@gmail.com>
2015-12-04 11:54:36 -0500

    refactoring away addclass to use better widget_tweaks version

--- annotations/templatetags/annotation_filters.py
+++ annotations/templatetags/annotation_filters.py
@@ -2,10 +2,6 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='addclass')
-def addclass(value, arg):
-    return value.as_widget(attrs={'class': arg})
-
 @register.filter(name='has_errors')
 def has_errors(form):
     return len(form.errors) > 0 and not isinstance(form.errors, list)
