35c1915e453b1f6178ba54dcd3202429e8ca4704
John Shen <john.da.shen@gmail.com>
2015-12-04 12:00:43 -0500

    refactoring away has_errors

--- annotations/templatetags/annotation_filters.py
+++ annotations/templatetags/annotation_filters.py
@@ -2,6 +2,4 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='has_errors')
-def has_errors(form):
-    return len(form.errors) > 0 and not isinstance(form.errors, list)
+# your filters here
