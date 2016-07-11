48bcf130c5355ca22e30d3303b74c075eb7d6901
Max Leiserson <max.leiserson@gmail.com>
2016-07-11 12:26:46 -0400

    merge migrations

---
+++ annotations/migrations/0028_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('annotations', '0027_auto_20160628_1806'),
+        ('annotations', '0027_auto_20160621_1640'),
+    ]
+
+    operations = [
+    ]
