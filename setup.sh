bb308b8428b4aaa55ebc91c2951d4a8e546a7a01
Max Leiserson <max.leiserson@gmail.com>
2017-02-06 18:22:34 -0500

    fix setup script

--- setup.sh
+++ setup.sh
@@ -15,9 +15,9 @@ VARIANT_ANNOTATIONS=$DATA_DIR/variant-annotations
 # DOWNLOAD AND UNPACK DATA
 ################################################################################
 
-# wget http://compbio-research.cs.brown.edu/software/magi-annotations/data/archives/latest.tar.gz
-# tar -xvzf latest.tar.gz
-# rm latest.tar.gz
+wget http://compbio-research.cs.brown.edu/software/magi/data/archives/latest.tar.gz
+tar -xvzf latest.tar.gz
+rm latest.tar.gz
 
 ################################################################################
 # PROCESS THE GENOME AND CANCER TYPES
@@ -25,36 +25,36 @@ VARIANT_ANNOTATIONS=$DATA_DIR/variant-annotations
 cd $FIXTURES_DIR
 
 CANCERS_FIXTURE=$FIXTURES_DIR/icgc-tcga-cancers.json
-# python cancersToFixtures.py \
-# 	-cff $DATA_DIR/icgc-tcga-cancers.tsv \
-# 	-o $CANCERS_FIXTURE
+python cancersToFixtures.py \
+	-cff $DATA_DIR/icgc-tcga-cancers.tsv \
+	-o $CANCERS_FIXTURE
 
 GENOME_FIXTURE=$FIXTURES_DIR/hg19-genome.json
-# python genomeToFixture.py \
-# 	-gf $DATA_DIR/genome/hg19_genes_list.tsv \
-# 	-o $GENOME_FIXTURE
+python genomeToFixture.py \
+	-gf $DATA_DIR/genome/hg19_genes_list.tsv \
+	-o $GENOME_FIXTURE
 
 ################################################################################
 # PROCESS PROTEIN-PROTEIN INTERACTIONS WITH ANNOTATIONS
 ################################################################################
-# python networksToFixture.py \
-# 	-gf $GENOME_FIXTURE \
-# 	-o all-networks \
-# 	-nf $PPI_DIR/iref9-annotated.tsv \
-# 	    $PPI_DIR/hprd-annotated.tsv \
-# 		$PPI_DIR/hint-annotated.tsv \
-# 		$PPI_DIR/multinet.tsv
+python networksToFixture.py \
+	-gf $GENOME_FIXTURE \
+	-o all-networks \
+	-nf $PPI_DIR/iref9-annotated.tsv \
+	    $PPI_DIR/hprd-annotated.tsv \
+		$PPI_DIR/hint-annotated.tsv \
+		$PPI_DIR/multinet.tsv
 
 ################################################################################
 # PROCESS VARIANT ANNOTATIONS
 ################################################################################
-# python annotationsToFixture.py \
-# 	-af $VARIANT_ANNOTATIONS/pmc-search/tcga-pancancer-stad-pmc-search-magi-format.tsv \
-# 	    $VARIANT_ANNOTATIONS/docm/docm-variants-magi-format.tsv \
-# 	-s "PMC Search" DoCM \
-# 	-cff $CANCERS_FIXTURE -o pmc-search-docm \
-# 	--heritable "" Somatic \
-# 	-gf $GENOME_FIXTURE
+python annotationsToFixture.py \
+	-af $VARIANT_ANNOTATIONS/pmc-search/tcga-pancancer-stad-pmc-search-magi-format.tsv \
+	    $VARIANT_ANNOTATIONS/docm/docm-variants-magi-format.tsv \
+	-s "PMC Search" DoCM \
+	-cff $CANCERS_FIXTURE -o pmc-search-docm \
+	--heritable "" Somatic \
+	-gf $GENOME_FIXTURE
 
 ################################################################################
 # LOAD THE DATA INTO POSTGRES
@@ -62,10 +62,10 @@ GENOME_FIXTURE=$FIXTURES_DIR/hg19-genome.json
 cd $PROJECT_DIR
 
 # Load the genes and cancers
-# python manage.py loaddata $GENOME_FIXTURE $CANCERS_FIXTURE $FIXTURES_DIR/*genes.json
+python manage.py loaddata $GENOME_FIXTURE $CANCERS_FIXTURE $FIXTURES_DIR/*genes.json
 
 # Load the variant annotations
-# python manage.py loaddata $FIXTURES_DIR/pmc-search-docm*.json
+python manage.py loaddata $FIXTURES_DIR/pmc-search-docm*.json
 
 # Load the protein-protein interactions
 python manage.py loaddata $FIXTURES_DIR/all-networks*
